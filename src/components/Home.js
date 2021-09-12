e1d0465f929366d2ee9f1545b5ef7ba0866d9224
Lakshay Agarwal <lakshay.agar@gmail.com>
2021-09-12 15:16:26 +0530

    weekend-work

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
-import React,{useState,useEffect} from 'react'; //useState one of the basic hooks,to set state in a functional component
+import React from 'react';
+ //useState one of the basic hooks,to set state in a functional component
 
 
 
@@ -7,6 +8,11 @@ import { POSTER_SIZE,BACKDROP_SIZE,IMAGE_BASE_URL } from '../config';
 
 //components
 import HeroImage from './HeroImage';
+import Grid from './Grid';
+import Thumb from './Thumb';
+import Spinner from './Spinner';
+import SearchBar from './SearchBar';
+import Button from './Button';
 
 //Hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
@@ -17,20 +23,40 @@ import  NoImage from '../images/no_image.jpg'
 
 const Home = ()=>{
     
-    const{state,loading,error} = useHomeFetch();
+    const{state,loading,error,searchTerm,setSearchTerm , setIsLoadingMore} = useHomeFetch();
     console.log(state);
+
+    if(error) return <div>Something went wrong ...</div>
 
     return (
         //fragment
         <>
-        {state.results[0] ? (
+        {!searchTerm && state.results[0] ? (
             <HeroImage
                 image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
                 title={state.results[0].original_title}
                 text={state.results[0].overview}
             /> 
-        ) : null
-        })
+        ) : null}
+
+        <SearchBar setSearchTerm = {setSearchTerm}/>
+        <Grid header={searchTerm?'Search Result ': 'Popular Movies'}>
+            {state.results.map(movie =>(
+                <Thumb
+                    key={movie.id}
+                    clickable
+                    image = {
+                        movie.poster_path ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                        : NoImage
+                    }
+                    movieId= {movie.id}
+                 />
+            ))}
+        </Grid>
+        {loading && <Spinner/>}
+        {state.page<state.total_pages && !loading && (
+            <Button text ='Load More' callback={()=> setIsLoadingMore(true)}/>
+        )}
         </>
     );
 };
--- src/hooks/useHomeFetch.js
+++ src/hooks/useHomeFetch.js
@@ -12,9 +12,12 @@ const initialState={
 }
 
 export const useHomeFetch = ()=>{
+    const [searchTerm,setSearchTerm] = useState('');
     const [state,setState] = useState(initialState);
     const [loading ,setLoading] = useState(false);
     const [error,setError]=useState(false);
+    const [isLoadingMore,setIsLoadingMore] = useState(false);
+
 
     const fetchMovies = async(page,searchTerm = "")=>{
         try{
@@ -36,10 +39,19 @@ export const useHomeFetch = ()=>{
         setLoading(false);
     };
 
-    //Initial Render
+    //Initial Render and Search 
     useEffect(()=>{ // using this hook to grab data from the movie database 
-        fetchMovies(1)
-    },[])
+        setState(initialState);
+        fetchMovies(1,searchTerm);
+    },[searchTerm])
 
-    return { state,loading,error};
+    //Load more 
+    useEffect(() => {
+        if(!isLoadingMore) return ;
+
+        fetchMovies(state.page +1 , searchTerm);
+        setIsLoadingMore(false);
+    },[isLoadingMore,searchTerm,state.page])
+
+    return { state,loading,error,searchTerm,setSearchTerm , setIsLoadingMore};
 }
